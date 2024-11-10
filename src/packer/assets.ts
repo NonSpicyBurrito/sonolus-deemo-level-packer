544f2933a83e392c4c58cf4622f19c7387856944
Burrito <burritoyangster@gmail.com>
2024-11-10 11:18:19 +0800

    Implement Deemo (#1)

--- src/packer/assets.ts
+++ src/packer/assets.ts
@@ -13,3 +13,10 @@ export const repository = Object.entries(
     filename: path.slice(path.lastIndexOf('/') + 1),
     buffer: buffer as ArrayBuffer,
 }))
+
+export { default as engineConfiguration } from 'sonolus-deemo-engine/EngineConfiguration?arraybuffer'
+export { default as enginePlayData } from 'sonolus-deemo-engine/EnginePlayData?arraybuffer'
+export { default as enginePreviewData } from 'sonolus-deemo-engine/EnginePreviewData?arraybuffer'
+export { default as engineThumbnail } from 'sonolus-deemo-engine/EngineThumbnail?arraybuffer'
+export { default as engineTutorialData } from 'sonolus-deemo-engine/EngineTutorialData?arraybuffer'
+export { default as engineWatchData } from 'sonolus-deemo-engine/EngineWatchData?arraybuffer'
